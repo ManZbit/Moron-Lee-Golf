38b68050d68f1437b5fe6043314c9b9912c79957
Manuel Choi <manuchoi@hotmail.com>
2022-02-22 17:27:59 -0300

    se sacaron los precios, ajuste de las columnas en media query

---
+++ js/script.js
@@ -0,0 +1,32 @@
+const toggle = () => document.body.classList.toggle("darkmode");
+
+const changeIcon = (icon) => {
+  icon.classList.toggle("fa-sun");
+  icon.classList.toggle("fa-moon"); 
+};
+
+/* let darkMode = localStorage.getItem("darkMode");
+const darkModeToggle = document.querySelector("#dark-mode-toggle");
+
+const enableDarkMode = () => {
+  localStorage.setItem("darkMode", "enabled");
+};
+
+const disableDarkMode = () => {
+  localStorage.setItem("darkMode", null);
+};
+
+if (darkMode === "enabled") {
+  enableDarkMode();
+}
+
+darkModeToggle.addEventListener("click", () => {
+  darkMode = localStorage.getItem("darkMode");
+
+  if (darkMode !== "enabled") {
+    enableDarkMode();
+  } else {
+    disableDarkMode();
+  }
+});
+ */
